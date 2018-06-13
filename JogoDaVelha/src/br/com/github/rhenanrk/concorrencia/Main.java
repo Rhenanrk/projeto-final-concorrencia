e3d92cb659d1fd3179bc95cfc3095bf144b8d870
Rhenan Konrad <rhenan_konrad@hotmail.com>
2018-06-13 19:47:04 -0300

    Upload da classe que implementa o jogo

--- JogoDaVelha/src/br/com/github/rhenanrk/concorrencia/Main.java
+++ JogoDaVelha/src/br/com/github/rhenanrk/concorrencia/Main.java
@@ -7,6 +7,12 @@ package br.com.github.rhenanrk.concorrencia;
 public class Main {
 
     public static void main(String args[]) {
-
+        
+        //criacção dos paineis
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ControlaJogo().setVisible(true);
+            }
+        });
     }
 }
